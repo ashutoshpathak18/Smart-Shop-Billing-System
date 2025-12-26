d9432736768af86ebb3ff28ba7b88bf8ff5db575
ashutoshpathak18 <pathakashutosh679@gmail.com>
2025-12-26 20:22:55 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,12 @@
 print("Welcome to Smart Shop")
 print("Press 1 for owner Dashboard\nPress 2 for costumer Dashboard\n")
 ch=int(input())
+def show_prod():
+    print("Product\t\tPrice")
+    with open("D:\GitHub\Smart-Shop-Billing-System\prod_list.txt") as f:
+            for line in f:
+                prod,price = line.strip().split("|")
+                print(f"{prod}\t{price}")
 if ch==1:
     password=input("Enter the password for owner login :")
     if password=="owner123":
@@ -16,9 +22,30 @@ if ch==1:
             elif choice==2:
                 name=input("Enter the Product name :")
                 price=float(input("Enter the price of the product :"))
+                with open("D:\GitHub\Smart-Shop-Billing-System\prod_list.txt", "a") as f:
+                  f.write(f"{name}|{price}\n")
+                print("Product added successfully....✅\n")
 
             elif choice==3:
-                id=int(input("Enter the product id to remove from product list"))
+                show_prod
+                remove_prod=""
+                id=input("Enter the name of product  to remove from product list")
+                with open("D:\GitHub\Smart-Shop-Billing-System\prod_list.txt") as f:
+                 for line in f:
+                  prod,price = line.strip().split("|")
+                  if prod==id:
+                      remove_prod= prod
+                      break
+                try:
+                    with open("D:\GitHub\Smart-Shop-Billing-System\prod_list.txt", "r") as file:
+                       lines = file.readlines()
+                    with open("D:\GitHub\Smart-Shop-Billing-System\prod_list.txt", "w") as file:
+                      for line in lines:
+                        if line != remove_prod:
+                            file.write(line) 
+                except FileNotFoundError:
+                    print("Product not found")
+
             elif choice==4:
                 break
             else:
@@ -26,6 +53,6 @@ if ch==1:
 
     else:
         print("Invalid Login")
-elif ch==2:
+#elif ch==2:
 else:
     print("Entered invalid choise")
